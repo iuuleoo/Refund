077eded6b46928b416ca7b5d0ec7cfff5ad3b2a2
Leo <leooproencaaa@gmail.com>
2025-12-08 15:35:12 -0300

    Button Enviar

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -4,19 +4,32 @@ import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/input"
 import { Select } from "../components/Select"
 import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
+
 
 
 export function Refund(){
+    const [name, setName] = useState("")
+    const [amount, setAmount ] = useState("")
     const [category, setCategory] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [filename, setFilename] = useState<File | null>(null)
+
+
+    function onSubmit(e: React.FormEvent) {
+        e.preventDefault()
+       
+        console.log(name, amount, category, filename)
+    }
     
-    return <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg-min-w-[512px]">
+    return <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg-min-w-[512px]">
         <header>
             <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
             <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesas para solicitar reembolso.</p>
         </header>
 
         <Input required
-        legend="Nome da solicitação"
+        legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)}
         />
 
      <div className="flex gap-4">
@@ -29,11 +42,16 @@ export function Refund(){
      ))}
     </Select>
 
-    <Input legend="Valor" required ></Input>
+    <Input legend="Valor" required value={amount} onChange={(e) => setAmount(e.target.value)} ></Input>
 
     </div>
 
-    <Upload />
+    <Upload filename={filename && filename.name}
+    onChange={(e) => e.target.files && setFilename(e.target.files[0])} />
+
+    <Button type="submit" isLoading={isLoading}>
+        Enviar
+    </Button>
 
     </form>
 }
