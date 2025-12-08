6f1f38af0dd0f8464dcf935b0a2e3d403f8aaa71
Leo <leooproencaaa@gmail.com>
2025-12-08 15:02:56 -0300

    Populando Select com Categorias

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
+
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/input"
 import { Select } from "../components/Select"
 
 
 export function Refund(){
+    const [category, setCategory] = useState("")
+    
     return <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg-min-w-[512px]">
         <header>
             <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
@@ -12,6 +17,14 @@ export function Refund(){
         <Input required
         legend="Nome da solicitação"
         />
-        <Select required legend="Categoria" />
+        <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
+  {CATEGORIES_KEYS.map((category) => (
+    <option key={category} value={category}>
+      {CATEGORIES[category].name}
+    </option>
+     ))}
+    </Select>
+
+
     </form>
 }
---
+++ src/utils/categories.ts
@@ -0,0 +1,38 @@
+import foodSvg from "../assets/food.svg"
+import othersSvg from "../assets/others.svg"
+import servicesSvg from "../assets/services.svg"
+import transportSvg from "../assets/transport.svg"
+import accommodationSvg from "../assets/accommodation.svg"
+
+export const CATEGORIES = {
+
+    others: {
+        name: "Outros",
+        icon: othersSvg
+    },
+
+    food: {
+        name: "Alimentação",
+        icon: foodSvg
+    },
+
+     services: {
+        name: "Serviços",
+        icon: servicesSvg
+    }
+    ,
+
+    transport: {
+        name: "Transporte",
+        icon: transportSvg
+    },
+    accommodation: {
+        name: "Hospedagem",
+        icon: accommodationSvg
+    
+    },
+   
+}
+
+export const CATEGORIES_KEYS = Object.keys(CATEGORIES) as Array<keyof typeof CATEGORIES>;
+
