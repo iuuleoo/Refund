fc934d4b2a23830a5890cae7f8533f5279b6e5df
Leo <leooproencaaa@gmail.com>
2025-12-08 15:42:12 -0300

    Confirm

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useNavigate } from "react-router";
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/input"
@@ -15,11 +16,14 @@ export function Refund(){
     const [isLoading, setIsLoading] = useState(false)
     const [filename, setFilename] = useState<File | null>(null)
 
+    const navigate = useNavigate()
+
 
     function onSubmit(e: React.FormEvent) {
         e.preventDefault()
        
         console.log(name, amount, category, filename)
+        navigate("/confirm", {state: {fromSubmit: true }})
     }
     
     return <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg-min-w-[512px]">
