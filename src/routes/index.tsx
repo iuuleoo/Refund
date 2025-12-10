63a6cebb7f8934466da8934c56a6c8bb3e222fe0
Leo <leooproencaaa@gmail.com>
2025-12-10 18:06:00 -0300

    Role do Usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,27 @@ import { EmployeeRoutes } from "./EmployeeRoutes";
 import { Loading } from "../components/Loading";
 
 const isLoading = false
+// const session = undefined
+
+
+const session = {
+  user: {
+    role: "employee",
+  },
+}
 
 export function Routes() {
+  function Route(){
+    switch (session?.user.role) {
+      case "employee":
+            return <EmployeeRoutes />
+            case "manager":
+            return <ManagerRoutes />
+    
+      default:
+        return <AuthRoutes />
+    }
+  }
 
   if(isLoading){
     return <Loading />
@@ -16,7 +35,7 @@ export function Routes() {
 
     return (
         <BrowserRouter>
-          <AuthRoutes />
+          <Route />
         </BrowserRouter>
     )
 }
